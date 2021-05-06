b548086cd1cf69342905f710898d42897415cf96
jana sanjakdar <jana.sanjakdar@hotmail.com>
2021-05-06 20:24:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-let divide = (a=2000,b=100) => a/b
-let square = x => x * x
-let add (a,b) => a+b
+let divide = function () {
+  return 2000/100
+}
+let square = number => number * number
+
+let add = (param1, param2) => param1 + param2
